5b425c189decaf36ca0086865cfe030e835d7a3c
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-07-26 01:40:13 -0500

    scrape content, api openai, and store in MongoDb

--- daily_task/fetch_and_send.py
+++ daily_task/fetch_and_send.py
@@ -2,13 +2,27 @@ import requests
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta, timezone
+from newspaper import Article
+from openai import OpenAI
+from pymongo import MongoClient
+from pathlib import Path
+import json
+
+def clean(text):
+    lines = []
+    for line in text.splitlines():
+        line = line.strip()
+        if line:
+            lines.append(line)
+    return "\n".join(lines)
 
 # get links
 utc_datetime = datetime.now(timezone.utc)
 utc_date = utc_datetime.date()
-utc_yesterday = utc_date - timedelta(days=1)
+utc_yesterday = str(utc_date - timedelta(days=1))
 url = "https://api.thenewsapi.com/v1/news/top"
-load_dotenv()
+root_dir = Path(__file__).resolve().parents[1]
+load_dotenv(root_dir / ".env")
 
 params = {
     "api_token": os.getenv("THENEWSAPI_TOKEN"),
@@ -48,5 +62,27 @@ for article in articles:
     links["Politics"].append(article["url"])
 
 
-
-
+#scrape website and send to openai and store data in MongoDB
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+mongo_client = MongoClient("localhost", 27017)
+for category in links:
+    for link in links[category]:
+        article = Article(link)
+        article.download()
+        article.parse()
+        text = article.text
+        text = clean(text)
+        response = openai_client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a summarizer for a 5 year old.  Only respond in strict JSON with the fields: Title, Short Summary, What, When, Where, Who, How. Do not add any extra text, explanation, or markdown like ```json."},
+                {"role": "user", "content": text}
+            ]
+        )
+        reply = json.loads(response.choices[0].message.content)
+        reply["date"] = utc_yesterday
+        reply["url"] = link
+        reply["categories"] = category
+        db = mongo_client.testdb
+        collection = db.summaries
+        collection.insert_one(reply)
